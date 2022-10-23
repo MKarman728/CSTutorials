e4cb03e0bc7e56cd8fbcab65d223eb5beb828566
=mkarman728 <mkarman08@gmail.com>
2022-10-22 22:34:13 -0700

    Challenge Problems

---
+++ Challenge_13_Variables.js
@@ -0,0 +1,14 @@
+/* =========== *
+* Challenge 13 *
+* ============ */
+
+function divisibleByFivePairSum(array){
+    // your code here...
+  }
+  
+  
+  //Uncomment the lines below to test your function:
+  
+  // console.log(divisibleByFivePairSum([1, 5, 2, 0, 4])); // => [ [ 0, 4 ], [ 1, 3 ] ]
+  // console.log(divisibleByFivePairSum([13, 22, 8, -3, 12])); // => [[ 0, 1 ], [ 0, 3 ], [ 0, 4 ], [ 1, 2 ], [ 2, 3 ], [ 2, 4 ]]
+  
